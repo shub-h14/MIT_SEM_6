fff41d9ab47ab70fe122961214dd88858323d55b
Shubhangi <meetshubhangi14@gmail.com>
2025-05-24 09:03:16 +0530

    Add files via upload

---
+++ CD LAB/lab5/Q6/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("ENTER A NUMBER: "); // HELLO THIS IS AN UPPERCASE COMMENT
+    scanf("%d", &num);
+    
+    /* This is a multi-line comment with
+    UPPERCASES CHARACTERS in it*/
+    
+    printf("YOU ENTERED: %d\n", num);
+
+    return 0;
+}
